9b4bc6987b402bb65d0dad55fab79684590e4f51
Amir <amirrostami808692@gmail.com>
2025-12-18 20:52:11 +0330

    added tarkib function

--- practice/math.cpp
+++ practice/math.cpp
@@ -55,7 +55,15 @@ long kmm(long a, long b)
 
 long tarkib(int n, int k) 
 {
-
+if (k > n) return 0;
+    if (k == 0 || k == n) return 1;
+    if (k > n / 2) k = n - k;
+   
+    long res = 1;
+    for (int i = 1; i <= k; i++) {
+        res = res * (n - i + 1) / i;
+    }
+    
 }
    
 
